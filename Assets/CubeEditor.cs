b9ca1b9959da3eb5051867ab5ee20ab77d9cfce3
Agustin Rambaldo <agustin.rc97@gmail.com>
2021-04-16 14:16:40 -0300

    Initial commit

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RandomColor))]
+public class CubeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        RandomColor randomColor = (RandomColor)target; 
+
+        if (GUILayout.Button("Generate Color"))
+        {
+            randomColor.GenerateColor();
+            Debug.Log("Presione generate color");
+        }
+    }
+}
+ 
